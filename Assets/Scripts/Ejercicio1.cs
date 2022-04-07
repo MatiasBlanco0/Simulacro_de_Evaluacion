4f8a6babbd5dccbb7cad5e79f6c182e2a6a89388
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-04-07 14:59:08 -0300

    Update Ejercicio1.cs

--- Assets/Scripts/Ejercicio1.cs
+++ Assets/Scripts/Ejercicio1.cs
@@ -6,18 +6,18 @@ public class Ejercicio1 : MonoBehaviour
 {
     //Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
 
-    public int num1 = 0;
-    public int num2 = 0;
+    public int numero1 = 0;
+    public int numero2 = 0;
     // Start is called before the first frame update
     void Start()
     {
-        if(num1 == num2)
+        if(numero1 == numero2)
         {
-            Debug.Log("Los numeros " + num1 + " y " + num2 + " son iguales");
+            Debug.Log("Los numeros " + numero1 + " y " + numero2 + " son iguales");
         }
         else
         {
-            Debug.Log("Los numeros " + num1 + " y " + num2 + " no son iguales ");
+            Debug.Log("Los numeros " + numero1 + " y " + numero2 + " no son iguales ");
         }
     }
 
