ac29617e4859e82fbc09c5f107383bb66b7c4eeb
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-04-07 14:58:16 -0300

    Programacion del Ejercicio1

--- Assets/Scripts/Ejercicio1.cs
+++ Assets/Scripts/Ejercicio1.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class Ejercicio1 : MonoBehaviour
 {
+    //Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
+
+    public int num1 = 0;
+    public int num2 = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(num1 == num2)
+        {
+            Debug.Log("Los numeros " + num1 + " y " + num2 + " son iguales");
+        }
+        else
+        {
+            Debug.Log("Los numeros " + num1 + " y " + num2 + " no son iguales ");
+        }
     }
 
     // Update is called once per frame
